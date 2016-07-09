3e048e6871d8ee35c31066dc4a51dc7550cfdec7
Lion <maolion.j@gmail.com>
2016-07-09 21:56:40 +0800

    😳

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import * as React from 'react';
 import AppBarLayoutAndroid from './components/appbar-layout-android/appbar-layout-android';
 import CoordinatorLayoutAndroid from './components/coordinator-layout-android/coordinator-layout-android';
 import NestedScrollViewAndroid from './components/nested-scroll-view-android/nested-scroll-view-android';
 import TabLayoutAndroid from './components/tab-layout-android/tab-layout-android';
 
+(global as any).React = React;
 
 export { 
     AppBarLayoutAndroid, 
