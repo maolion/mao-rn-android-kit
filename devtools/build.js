d2e3d04fabab8082b761af29655119713863c80e
Lion <maolion.j@gmail.com>
2016-09-09 16:13:58 +0800

    fix excludes options not work bug

--- devtools/build.js
+++ devtools/build.js
@@ -17,11 +17,17 @@ tsc(Constants.SOURCE_DIR, {
             console.log('generator mao-rn-android-kit.d.ts');
             dtsGenerator({
                 name: 'mao-rn-android-kit',
-                baseDir: Constants.SOURCE_DIR,
-
-                main: 'mao-rn-android-kit/index',
-                externs: [
-                    './interfaces.d.ts', 
+                baseDir: Constants.CWD,
+                files: [
+                    Path.join(Constants.SOURCE_DIR, 'index.ts'),
+                    Path.join(Constants.CWD, 'typings/index.d.ts'),
+                    Path.join(Constants.CWD, 'typings/interfaces.d.ts')
+                ],
+                main: 'mao-rn-android-kit/src/index',
+                excludes: [
+                    'node_modules/**/*.d.ts',
+                    'typings/globals/**/*.d.ts',
+                    'typings/index.d.ts'
                 ],
                 out: Path.join(Constants.OUTPUT_DIR, 'index.d.ts') 
             })
@@ -29,7 +35,6 @@ tsc(Constants.SOURCE_DIR, {
         });
     })
     .then(() => {
-        FS.copySync(Path.join(Constants.CWD, 'typings/interfaces.d.ts'), Path.join(Constants.OUTPUT_DIR, 'interfaces.d.ts'));
         process.exit(0);
     })
     .fail(reason => {
