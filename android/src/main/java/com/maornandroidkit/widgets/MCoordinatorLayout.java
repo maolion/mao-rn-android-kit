cfb11abbb59021fcf113b7803283ffec01473fbd
Lion <maolion.j@gmail.com>
2016-10-27 08:42:53 +0800

    CoordinatorLayout 添加新接口， NestedScrollView 滚动问题修复, deploy v0.3.15

---
+++ android/src/main/java/com/maornandroidkit/widgets/MCoordinatorLayout.java
@@ -0,0 +1,55 @@
+package com.maornandroidkit.widgets;
+
+
+import android.content.Context;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MCoordinatorLayout extends CoordinatorLayout{
+    private final static int RESET_BEHOVIOR_Y_POS = -10000000;
+    private boolean mNestedScrollEnabled = true;
+
+    public MCoordinatorLayout(Context context) {
+        super(context);
+    }
+
+    public MCoordinatorLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setScrollingViewBehavior(View view) {
+        try {
+            CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) view.getLayoutParams();
+            params.setBehavior(new AppBarLayout.ScrollingViewBehavior());
+            view.requestLayout();
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+    }
+
+    public void resetBehavior(AppBarLayout appbar, boolean smoothly) {
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) appbar.getLayoutParams();
+        AppBarLayout.Behavior behavior = (AppBarLayout.Behavior) params.getBehavior();
+        if (!smoothly) {
+            int[] consumed = new int[2];
+            behavior.onNestedPreScroll(this, appbar, null, 0, RESET_BEHOVIOR_Y_POS, consumed);
+        } else {
+            behavior.onNestedFling(this, appbar, null, 0, RESET_BEHOVIOR_Y_POS, true);
+        }
+    }
+
+    // @Override
+    // public boolean onStartNestedScroll(View child, View target, int nestedScrollAxes) {
+    //     return mNestedScrollEnabled && super.onStartNestedScroll(child, target, nestedScrollAxes);
+    // }
+
+    public boolean isNestedScrollEnabled() {
+        return mNestedScrollEnabled == true;
+    }
+
+    public void setNestedScrollEnabled(boolean enabled) {
+        mNestedScrollEnabled = enabled;
+    }
+}
