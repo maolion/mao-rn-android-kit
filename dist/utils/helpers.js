ebecc4f8ce3f91989146717e72d94403bd59f669
Lion <maolion.j@gmail.com>
2016-11-04 08:03:32 +0800

    取消dist目录的提交跟踪

--- dist/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-export function foramt(template, data) {
-    if (!data) {
-        return template;
-    }
-    return template.replace(/\{([^\}]+)\}/g, function (mStr, cStr1) {
-        return data.hasOwnProperty(cStr1) ? data[cStr1] : cStr1;
-    });
-}
